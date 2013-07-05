a116e72421d8ec86a79135df819d3527c5aec6c4
laiyu fang <laiyufang@115.0.167.190.d.dyn.codetel.net.do>
2013-07-05 08:07:28 +0800

    Initial Commit

---
+++ WXWeibo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WXWeibo
+//
+//  Created by laiyu fang on 13-7-5.
+//  Copyright (c) 2013年 laiyu fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WXWeibo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WXWeibo
+//
+//  Created by laiyu fang on 13-7-5.
+//  Copyright (c) 2013年 laiyu fang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
